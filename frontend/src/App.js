169407af62ce0c428d33e49097556347151f948a
tx <tamas.knisz@gmail.com>
2021-02-05 20:10:51 +0100

    InfoDisplay component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import InfoDisplay from "./components/InfoDisplay";
 
 function App() {
     return (
-        <div>
-            <h1>Hydration Tracker</h1>
+        <div className="app__container">
+            <div className="app__title">
+                <h1>Hydration Tracker</h1>
+            </div>
+
+            <InfoDisplay>
+                <h3>0.75L</h3>
+                <p>Total water drunk</p>
+            </InfoDisplay>
+
+            <InfoDisplay>
+                <h3>3</h3>
+                <p>Day streak</p>
+            </InfoDisplay>
+            
+
+            <div className="user">
+
+            </div>
+
+            <div className="message">
+                <h4>Some message</h4>
+            </div>
+
+            <div className="volumes">
+                <div>100ml</div>
+                <div>250ml</div>
+                <div>350ml</div>
+            </div>
+
+            <div className="cta-buttons">
+                <div>PLUS</div>
+                <div>MINUS</div>
+            </div>
+
+
         </div>
     );
 }
---
+++ frontend/src/components/InfoDisplay.jsx
@@ -0,0 +1,13 @@
+
+
+const InfoDisplay = ({children}) => {
+    return(
+        <>
+            <div className="tracker">
+                {children}
+            </div>
+        </>
+    )
+}
+
+export default InfoDisplay
