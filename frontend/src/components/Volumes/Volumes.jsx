5049b3fd25bcf791dc9a71ea025ef218053e718a
tx <tamas.knisz@gmail.com>
2021-02-06 11:38:42 +0100

    Volume buttons style updated

--- frontend/src/components/Volumes/Volumes.jsx
+++ frontend/src/components/Volumes/Volumes.jsx
@@ -2,7 +2,7 @@ import './Volumes.css'
 
 const Volumes = ({onVolumeInput}) => {
     return(
-        <div className="volumes__container">
+        <div className="volumes__container flex fl-jc-cent">
             <div className="volumes__button" id="v100" onClick={onVolumeInput}>100ml</div>
             <div className="volumes__button" id="v250" onClick={onVolumeInput}>250ml</div>
             <div className="volumes__button" id="v350" onClick={onVolumeInput}>350ml</div>
