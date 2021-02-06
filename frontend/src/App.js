8b161938a11279ae2b999341fde1fec3f6201dbf
tx <tamas.knisz@gmail.com>
2021-02-06 11:35:38 +0100

    Volumes component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,41 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import InfoDisplay from "./components/InfoDisplay/InfoDisplay";
+import InfoDisplay from './components/InfoDisplay/InfoDisplay';
+import Volumes from './components/Volumes/Volumes';
 
 function App() {
+
+    const [volume, setVolume] = useState(100)
+
+    useEffect(() => {
+        console.log({volume})
+    }, [volume])
+
+    const onVolumeInput = (e) => {
+        const { id } = e.target
+
+        switch(id) {
+            case 'v100':
+                setVolume(100)
+                break;
+            case 'v250':
+                setVolume(250)
+                break;
+            case 'v350':
+                setVolume(350)
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <div className="app__container">
             <div className="app__title">
                 <h1>Hydration Tracker</h1>
             </div>
 
-            <div className="app__tracker-wrapper">
+            <div className="app__tracker-wrapper flex fl-jc-se">
                 <InfoDisplay>
                     <h3>0.75L</h3>
                     <p>Total water drunk</p>
@@ -20,6 +47,10 @@ function App() {
                 </InfoDisplay>
             </div>
 
+            <div className="modal">
+
+            </div>
+
             <div className="user">
 
             </div>
@@ -28,11 +59,7 @@ function App() {
                 <h4>Some message</h4>
             </div>
 
-            <div className="volumes">
-                <div>100ml</div>
-                <div>250ml</div>
-                <div>350ml</div>
-            </div>
+            <Volumes onVolumeInput={onVolumeInput} />
 
             <div className="cta-buttons">
                 <div>PLUS</div>
---
+++ frontend/src/components/Volumes/Volumes.jsx
@@ -0,0 +1,13 @@
+import './Volumes.css'
+
+const Volumes = ({onVolumeInput}) => {
+    return(
+        <div className="volumes__container">
+            <div className="volumes__button" id="v100" onClick={onVolumeInput}>100ml</div>
+            <div className="volumes__button" id="v250" onClick={onVolumeInput}>250ml</div>
+            <div className="volumes__button" id="v350" onClick={onVolumeInput}>350ml</div>
+        </div>
+    )
+}
+
+export default Volumes
